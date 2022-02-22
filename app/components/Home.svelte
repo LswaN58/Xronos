4cc39731cbc75f1069d709b4a4ab1c5a75f445cc
Luke Swanson <lwswanson2@wisc.edu>
2022-02-21 19:54:05 -0600

    Updates to homepage.

--- app/components/Home.svelte
+++ app/components/Home.svelte
@@ -1,17 +1,41 @@
 <page>
-    <actionBar title="Home" />
-    <gridLayout>
-        <label class="info">
-            <formattedString>
-                <span class="fas" text="&#xf135;" />
-                <span text=" {message}" />
-            </formattedString>
-        </label>
-    </gridLayout>
+    <actionBar title="Xronos" />
+
+    <tabView>
+        <tabViewItem title="Timers">
+            <gridLayout columns="*,120" rows="70,*">
+                <textField col="0" row="0" bind:text={textFieldValue} hint="Type a timer time..." editable="true" on:returnPress="{onButtonTap}" />
+                <button col="1" row="0" text="Add Timer" on:tap="{onButtonTap}" />
+
+                <listView items="{timers}" on:itemTap="{onItemTap}" row="1" colSpan="2">
+                    <!-- <Template let:item>
+                        <label text="{item.name}" textWrap="true" />
+                    </Template> -->
+                </listView>
+            </gridLayout>
+        </tabViewItem>
+        <tabViewItem title="Clocks">
+            <label textWrap="true">This tab shows the user's clocks.</label>
+        </tabViewItem>
+    </tabView>
 </page>
 
 <script lang="ts">
-    let message: string = "Blank Svelte Native App"
+    // import {Template} from 'svelte-native/components'
+
+    let timers: any[] = []
+    let textFieldValue: string = ""
+
+    function onItemTap(args:any) {
+        console.log(`Item ${timers[args.index].name} at index: ${args.index} was tapped`);
+    }
+
+    function onButtonTap() {
+        if (textFieldValue === "") return;
+        console.log(`New timer added: ${textFieldValue}.`);
+        timers = [{name : textFieldValue}, ...timers];
+        textFieldValue = "";
+    }
 </script>
 
 <style>
